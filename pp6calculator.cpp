1a46ad700daa8f31d12660a6d4f420f4f8049337
Ben Morgan <bmorgan.warwick@gmail.com>
2013-02-11 15:01:35 +0000

    Implement pp6calculator Exercise 2 solution
    Query user to enter two numbers, and report product of numbers to
    stdout.

--- pp6calculator.cpp
+++ pp6calculator.cpp
@@ -2,12 +2,19 @@
 
 int main() 
 {
-  // This is a comment
-  /* This is a
-     Multiline comment */
-
-  std::cout << "Hello World!" << std::endl;
-
+  // Declare the variables
+  double a(0), b(0);
+  
+  // Ask for two numbers from the user
+  std::cout << "Enter the first number:" << std::endl;
+  std::cin >> a;
+  
+  std::cout << "Enter the second number:" << std::endl;
+  std::cin >> b;
+  
+  // calculate the product and print the answer
+  std::cout << "The product of " << a << " and " << b << " is:  " << a*b << std::endl;
+  
   return 0;
 }
 
