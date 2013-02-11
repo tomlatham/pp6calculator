fc8903213c2e3cdf9746629141bc225a6dfc7500
Ben Morgan <bmorgan.warwick@gmail.com>
2013-02-11 17:31:08 +0000

    Implement pp6calculator Homework Exercise Part 1
    Refactor number input into getNumber function which performs error
    checking and returns the input number.
    
    Create proper menu of commands using numbers rather than symbols to
    select operations. Handle operations and quit option in single
    conditional block to allow easy future expansion.
    
    Neatify prompts and result output so that each are clearer, using
    style inspired by ipython interpreter.

--- pp6calculator.cpp
+++ pp6calculator.cpp
@@ -24,19 +24,47 @@ double divide(double a, double b)
   return a / b;
 }
 
+double getNumber()
+{
+  double res(0);
+
+  std::cin >> res;
+
+  while (!std::cin)
+  {
+    std::cout << "Error in input. Please re-enter." << std::endl;
+
+    // clear the buffer
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    // retry
+    std::cin >> res;
+  }
+
+  return res;
+}
+
 //----------------------------------------------------------------------
 // Main program
 //
 int main() 
 {
   // Declare the variables
-  double a(0), b(0), res(0);
+  double res(0);
   char op('\0');
   
   while (true)
   {
     // Ask the user what they want to do
-    std::cout << "Enter the operation you would like to perform (+, -, *, /) or q to quit:" << std::endl;
+    std::cout << "Enter the operation you would like to perform:" << std::endl;
+    std::cout << "1)  Addition" << std::endl;
+    std::cout << "2)  Subtraction" << std::endl;
+    std::cout << "3)  Multiplication" << std::endl;
+    std::cout << "4)  Division" << std::endl;
+    std::cout << "q)  Quit" << std::endl;
+    std::cout << ">> ";
+    
     std::cin >> op;
       
     // check for bad input
@@ -50,64 +78,44 @@ int main()
       continue;
     }
 
-    // check for quit
+    // Handle menu operation
     if (op == 'q')
     {
       break;
     }
-
-    // Ask for two numbers from the user
-    std::cout << "Enter the first number:" << std::endl;
-    std::cin >> a;
-      
-    // check for bad input
-    if(!std::cin)
+    else if((op == '1') || (op == '2') || (op == '3') || (op == '4') )
     {
-      std::cerr << "[error] Error in input" << std::endl;
-    
-      // clear the buffer
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-      continue;
-    }
-
-    std::cout << "Enter the second number:" << std::endl;
-    std::cin >> b;
-      
-    // check for bad input
-    if(!std::cin)
-    {
-      std::cerr << "[error] Error in input" << std::endl;
-    
-      // clear the buffer
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-      continue;
-    }
-
-    // calculate the result
-    if (op == '+')
-    {
-      res = add(a, b);
-    }
-    else if (op == '-')
-    {
-      res = subtract(a, b);
-    }
-    else if (op == '*')
-    {
-      res = multiply(a, b);
-    }
-    else if (op == '/')
-    {
-      if (b == 0)
+      // Arithmetic operations, so ask for two numbers from the user
+      double a(0), b(0);
+      std::cout << "Enter the first number: ";
+      a = getNumber();
+      std::cout << "Enter the second number: ";
+      b = getNumber();
+       
+      // calculate the result
+      if (op == '1')
       {
-        std::cerr << "[error] Divide by zero error" << std::endl;
-        continue;
+        res = add(a, b);
       }
-      else
+      else if (op == '2')
       {
-        res = divide(a, b);
+        res = subtract(a, b);
+      }
+      else if (op == '3')
+      {
+        res = multiply(a, b);
+      }
+      else if (op == '4')
+      {
+        if (b == 0)
+        {
+          std::cerr << "[error] Divide by zero error" << std::endl;
+          continue;
+        }
+        else
+        {
+          res = divide(a, b);
+        }
       }
     }
     else
@@ -117,10 +125,8 @@ int main()
       continue;
     }
       
-    // print the answer
-    std::cout << "The result of the operation " << op 
-              << " on " << a << " and " << b 
-              << " is:  " << res << std::endl;
+    // print the result
+    std::cout << "[result]: " << res << std::endl;
   }
   
   std::cout << "Thank you for using pp6calculator!" << std::endl;
