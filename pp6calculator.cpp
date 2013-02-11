7243baab388d7c29aab52fb494818ce962e595dd
Ben Morgan <bmorgan.warwick@gmail.com>
2013-02-11 15:58:42 +0000

    Implement pp6calculator Exercise 4 solution
    Wrap selection of arithmetic operator, numbers and result output in
    while loop to allow repeat operations. Exit loop on user entering
    'q' as operator.

--- pp6calculator.cpp
+++ pp6calculator.cpp
@@ -1,79 +1,105 @@
 #include <iostream>
+#include <limits>
 
 int main() 
 {
   // Declare the variables
   double a(0), b(0), res(0);
   char op('\0');
-
-  // Ask the user what they want to do
-  std::cout << "Enter the operation you would like to perform (+, -, *, /):" << std::endl;
-  std::cin >> op;
-
-  // check for bad input
-  if (!std::cin)
-  {
-    std::cerr << "[error] Error in input" << std::endl;
-    return 1;
-  }
   
-  // Ask for two numbers from the user
-  std::cout << "Enter the first number:" << std::endl;
-  std::cin >> a;
-
-  // check for bad input
-  if (!std::cin)
+  while (true)
   {
-    std::cerr << "[error] Error in input" << std::endl;
-    return 1;
-  }
-
-  std::cout << "Enter the second number:" << std::endl;
-  std::cin >> b;
-
-  // check for bad input
-  if (!std::cin)
-  {
-    std::cerr << "[error] Error in input" << std::endl;
-    return 1;
-  }
-
-  // calculate the result
-  if (op == '+')
-  {
-    res = a + b;
-  }
-  else if (op == '-')
-  {
-    res = a - b;
-  }
-  else if (op == '*')
-  {
-    res = a * b;
-  }
-  else if (op == '/')
-  {
-    if (b == 0)
+    // Ask the user what they want to do
+    std::cout << "Enter the operation you would like to perform (+, -, *, /) or q to quit:" << std::endl;
+    std::cin >> op;
+      
+    // check for bad input
+    if(!std::cin)
     {
-      std::cerr << "[error] Divide by zero error" << std::endl;
-      return 1;
+      std::cerr << "[error] Error in input" << std::endl;
+    
+      // clear the buffer
+      // note that the large argument of 'ignore' just specifies a 
+      // very large number of characters until a newline is encountered!
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      continue;
+    }
+
+    // check for quit
+    if (op == 'q')
+    {
+      break;
+    }
+
+    // Ask for two numbers from the user
+    std::cout << "Enter the first number:" << std::endl;
+    std::cin >> a;
+      
+    // check for bad input
+    if(!std::cin)
+    {
+      std::cerr << "[error] Error in input" << std::endl;
+    
+      // clear the buffer
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      continue;
+    }
+
+    std::cout << "Enter the second number:" << std::endl;
+    std::cin >> b;
+      
+    // check for bad input
+    if(!std::cin)
+    {
+      std::cerr << "[error] Error in input" << std::endl;
+    
+      // clear the buffer
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      continue;
+    }
+
+    // calculate the result
+    if (op == '+')
+    {
+      res = a + b;
+    }
+    else if (op == '-')
+    {
+      res = a - b;
+    }
+    else if (op == '*')
+    {
+      res = a * b;
+    }
+    else if (op == '/')
+    {
+      if (b == 0)
+      {
+        std::cerr << "[error] Divide by zero error" << std::endl;
+        continue;
+      }
+      else
+      {
+        res = a / b;
+      }
     }
     else
     {
-      res = a / b;
+      std::cerr << "[error] Operation '" << op << "' not recognised."
+                << std::endl;
+      continue;
     }
-  }
-  else
-  {
-    std::cerr << "[error] Operation '" << op << "' not recognised." << std::endl;
-    return 1;
+      
+    // print the answer
+    std::cout << "The result of the operation " << op 
+              << " on " << a << " and " << b 
+              << " is:  " << res << std::endl;    
   }
   
-  // print the answer
-  std::cout << "The result of the operation " << op 
-            << " on " << a << " and " << b 
-            << " is:  " << res << std::endl;
-  
+  std::cout << "Thank you for using pp6calculator!" << std::endl;
   return 0;
 }
 
