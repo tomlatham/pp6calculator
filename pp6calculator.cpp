94dcde8a3ad6a88ed85bbbe5bb3247cfbbce6eaf
Ben Morgan <bmorgan.warwick@gmail.com>
2013-02-11 15:34:31 +0000

    Implement pp6calculator Exercise 3 solution
    Provide interface to allow user to select one of the four arithmetic
    operations and then input two integers. Apply the operation to the
    numbers and report the result to stdout.
    
    Check input for errors at all stages, and report these to stderr before
    exiting with a failure.

--- pp6calculator.cpp
+++ pp6calculator.cpp
@@ -3,17 +3,76 @@
 int main() 
 {
   // Declare the variables
-  double a(0), b(0);
+  double a(0), b(0), res(0);
+  char op('\0');
+
+  // Ask the user what they want to do
+  std::cout << "Enter the operation you would like to perform (+, -, *, /):" << std::endl;
+  std::cin >> op;
+
+  // check for bad input
+  if (!std::cin)
+  {
+    std::cerr << "[error] Error in input" << std::endl;
+    return 1;
+  }
   
   // Ask for two numbers from the user
   std::cout << "Enter the first number:" << std::endl;
   std::cin >> a;
-  
+
+  // check for bad input
+  if (!std::cin)
+  {
+    std::cerr << "[error] Error in input" << std::endl;
+    return 1;
+  }
+
   std::cout << "Enter the second number:" << std::endl;
   std::cin >> b;
+
+  // check for bad input
+  if (!std::cin)
+  {
+    std::cerr << "[error] Error in input" << std::endl;
+    return 1;
+  }
+
+  // calculate the result
+  if (op == '+')
+  {
+    res = a + b;
+  }
+  else if (op == '-')
+  {
+    res = a - b;
+  }
+  else if (op == '*')
+  {
+    res = a * b;
+  }
+  else if (op == '/')
+  {
+    if (b == 0)
+    {
+      std::cerr << "[error] Divide by zero error" << std::endl;
+      return 1;
+    }
+    else
+    {
+      res = a / b;
+    }
+  }
+  else
+  {
+    std::cerr << "[error] Operation '" << op << "' not recognised." << std::endl;
+    return 1;
+  }
   
-  // calculate the product and print the answer
-  std::cout << "The product of " << a << " and " << b << " is:  " << a*b << std::endl;
+  // print the answer
+  std::cout << "The result of the operation " << op 
+            << " on " << a << " and " << b 
+            << " is:  " << res << std::endl;
   
   return 0;
 }
