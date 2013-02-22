636207453ffb2a52cf6e6d05ab9eff05a61f9811
Ben Morgan <bmorgan.warwick@gmail.com>
2013-02-22 16:39:45 +0000

    Implement pp6calculator Day 2 Exercise 5 solution
    Move function declarations into dedicated header file, leaving
    definitions in main for now. This is less than ideal, but provides a
    useful point at which to make a commit before moving forward with
    modularization.

--- pp6calculator.cpp
+++ pp6calculator.cpp
@@ -1,35 +1,10 @@
+// Standard Library
 #include <iostream>
 #include <limits>
 #include <cmath>
 
-//----------------------------------------------------------------------
-// Function Declarations
-//
-// - Arithmetic
-int add(double a, double b, double& answer);
-int subtract(double a, double b, double& answer);
-int multiply(double a, double b, double& answer);
-int divide(double a, double b, double& answer);
-
-// - Solvers
-int intercept(double m, double c, double& answer);
-int quadratic(double a, double b, double c, double& positiveRoot,
-              double& negativeRoot);
-
-// - Vectors
-double length(double x, double y, double z, double& vectorLength);
-double length(double t, double x, double y, double z, double& vectorLength);
-
-// - Physics
-double inv_mass(double e1, double px1, double py1, double pz1, 
-                double e2, double px2, double py2, double pz2,
-                double& invariantMass);
-
-// - Utility
-int swap(double& a, double &b);
-int basic_sort(double *arr, int size);
-void printArray(double *arr, int size);
-double getNumber();
+// This Project
+#include "PP6Math.hpp"
 
 //----------------------------------------------------------------------
 // Main program
